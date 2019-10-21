381f6edbed2ec8fd8b4a1f4b38bd347e819514fd
Jamie Luong <jamieluong03@gmail.com>
2019-10-20 22:13:59 -0700

    routed the public dir

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ var PORT = process.env.PORT || 8080;
 // data parsing with express
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
+// app.use('/assets/js', express.static('public'));
+// app.use('/assets/css', express.static('public'));
 
 // routing files
 require ("./routes/apiRoutes")(app);
