eeb219395f6acee17e529ec217148cbee080f3d0
Jamie Luong <jamieluong03@gmail.com>
2019-10-22 00:46:47 -0700

    added delete button icon

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -9,41 +9,17 @@ var $noteList = $("#tableData");
 
 // A function for getting all notes from the db
 var getNotes = function() {
-    $.ajax({ url: "/api/notes", method: "GET" })
-        .then(function(journalData){
-            console.log(journalData);
-            console.log($noteList);
-
-            for (var i=0; i < journalData.length; i++){
-                var listNote = $("<li class='list-group-item mt-4'>");
-
-                listNote.append(
-                    $("<h5>").text(journalData[i].title),
-                    $("<h5>").text(journalData[i].note)
-                );
-
-            $noteList.append(listNote);
-            
-            };
-            $noteList.append(journalData);
-        })
+   
 };
 
 // A function for saving a note to the db
-// var saveNote = function(note) {
-   
-
-// };
+// var saveNote = function(note) {};
 
 // A function for deleting a note from the db
-// var deleteNote = function(title) {
-  
-// };
+// var deleteNote = function(title) {};
 
 // // If there is an activeNote, display it, otherwise render empty inputs
-// var renderActiveNote = function() {
-  
-// };
+// var renderActiveNote = function() {};
 
 // Get the note data from the inputs, save it to the db and update the view
 var handleNoteSave = function() {
@@ -61,8 +37,13 @@ var handleNoteSave = function() {
       $noteTitle.val();
       $noteText.val();
   });
-
   document.getElementsByTagName('form')[0].reset();
+
+  // empties the div
+  $noteList.empty();
+
+  // recreates the div
+  getAndRenderNotes();
 };
 
 // Delete the clicked note
@@ -77,14 +58,9 @@ var handleNoteDelete = function(event) {
 
 // Sets the activeNote to and empty object and allows the user to enter a new note
 var handleNewNoteView = function() {
-    // event.preventDefault();
+
     document.getElementsByTagName('form')[0].reset();
 
-    // $.ajax({url: "/api/notes", method: "POST"})
-    // .then(function(){
-    //     $noteTitle.empty();
-    //     $noteText.empty();
-    // })
 };
 
 // If a note's title or text are empty, hide the save button
@@ -103,9 +79,26 @@ var handleRenderSaveBtn = function() {
 // };
 
 // Gets notes from the db and renders them to the sidebar
-// var getAndRenderNotes = function() {
-  
-// };
+var getAndRenderNotes = function() {
+    $.ajax({ url: "/api/notes", method: "GET" })
+    .then(function(journalData){
+        console.log(journalData);
+        console.log($noteList);
+
+        for (var i=0; i < journalData.length; i++){
+            var listNote = $("<li class='list-group-item mt-4'>");
+            listNote.append(
+                $("<button class='far fa-trash-alt float-right'>"),
+                $("<h4>").text(journalData[i].title),
+                // $("<h4>").text(journalData[i].note)
+            );
+
+        $noteList.append(listNote);
+        
+        };
+        $noteList.append(journalData);
+    })
+};
 
 $saveNoteBtn.on("click", handleNoteSave);
 $noteList.on("click", ".list-group-item", getNotes);
@@ -115,5 +108,4 @@ $noteTitle.on("keyup", handleRenderSaveBtn);
 $noteText.on("keyup", handleRenderSaveBtn);
 
 // Gets and renders the initial list of notes
-// getAndRenderNotes();
-getNotes();
+getAndRenderNotes();
